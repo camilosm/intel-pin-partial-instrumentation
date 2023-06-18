7123413f7ab080de130eed87292f14d3870ce697
Camilo <camilocsm@gmail.com>
2023-06-17 22:55:28 -0300

    added simple program to test pintool

---
+++ hello.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+void func1(){
+    printf("Hello");
+}
+
+void func2(){
+    printf(" ");
+}
+
+void func3(){
+    printf("World\n");
+}
+
+int main(){
+    func1();
+    func2();
+    func3();
+}
