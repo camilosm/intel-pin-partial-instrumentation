3eb379c42139119040b33972f5d3e97c21905708
Camilo <camilocsm@gmail.com>
2023-06-20 00:30:14 -0300

    removed time count instrumentation

--- pintool.cpp
+++ pintool.cpp
@@ -5,7 +5,6 @@
 #include <set>
 #include <map>
 #include <list>
-#include <chrono>
 #include <numeric>
 
 #include "pin.H"
@@ -25,17 +24,6 @@ KNOB<ADDRINT> KnobAddressEnd(KNOB_MODE_WRITEONCE, "pintool", "e", "0", "range fi
 std::set<ADDRINT> filter_addresses_set;
 
 std::map<ADDRINT, Instruction*> instruction_map;
-
-std::chrono::time_point<std::chrono::high_resolution_clock> time_start;
-VOID time_before(){
-    time_start = std::chrono::high_resolution_clock::now();
-}
-
-std::chrono::time_point<std::chrono::high_resolution_clock> time_end;
-VOID time_after(ADDRINT address){
-    time_end = std::chrono::high_resolution_clock::now();
-    instruction_map[address]->time += time_end - time_start;
-}
 
 VOID increment_count(ADDRINT address){
     instruction_map[address]->count++;
@@ -62,21 +50,15 @@ VOID Trace(TRACE trace, VOID* v){
             Instruction* instruction = new Instruction(address, mnemonic, function);
             instruction_map.insert(std::make_pair(address, instruction));
             INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(increment_count), IARG_ADDRINT, address, IARG_END);
-            if(INS_IsValidForIpointAfter(ins)){
-                INS_InsertCall(ins, IPOINT_BEFORE, AFUNPTR(time_before), IARG_END);
-                INS_InsertCall(ins, IPOINT_AFTER, AFUNPTR(time_after), IARG_ADDRINT, address, IARG_END);
-            }
         }
     }
 }
 
 void print_instruction_map(FILE* fp, bool group){
-    // milliseconds
-    int time_scale = 1e3;
     if(!group){
         for(auto pair : instruction_map){
             Instruction* instruction = pair.second;
-            fprintf(fp,"0x%lx:%s:%s:%f:%lu\n", instruction->address, instruction->mnemonic.c_str(), instruction->function.c_str(), instruction->time.count() * time_scale, instruction->count);
+            fprintf(fp,"0x%lx:%s:%s:%lu\n", instruction->address, instruction->mnemonic.c_str(), instruction->function.c_str(), instruction->count);
         }
     }
     else{
@@ -89,8 +71,7 @@ void print_instruction_map(FILE* fp, bool group){
         for(auto pair : instructions_grouped){
             std::string function = pair.first;
             std::list<Instruction*> instruction_list = pair.second;
-            std::chrono::duration<double> time_total = std::accumulate(instruction_list.begin(), instruction_list.end(), std::chrono::duration<double>(0), [](std::chrono::duration<double> current, Instruction* ins){return current + ins->time;});
-            fprintf(fp,"Function %s, %f, %lu:\n", function.c_str(), time_total.count() * time_scale, instruction_list.size());
+            fprintf(fp,"Function %s, %lu:\n", function.c_str(), instruction_list.size());
             for(auto instruction : instruction_list)
                 fprintf(fp,"0x%lx:%s:%lu\n", instruction->address, instruction->mnemonic.c_str(), instruction->count);
         }
